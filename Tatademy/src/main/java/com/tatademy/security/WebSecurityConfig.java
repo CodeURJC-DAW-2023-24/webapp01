cd547d8b10a90e6d3e183a75dd790fdfaa1c0cfb
p-torrecilla <p.torrecilla.2021@alumnos.urjc.es>
2024-02-24 16:29:57 +0100

    Beggining of login

--- Tatademy/src/main/java/com/tatademy/security/WebSecurityConfig.java
+++ Tatademy/src/main/java/com/tatademy/security/WebSecurityConfig.java
@@ -1,7 +1,35 @@
 package com.tatademy.security;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+ 
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+ 
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+         
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(anyRequest ->
+            anyRequest.requestMatchers("/").authenticated()
+            .anyRequest().permitAll()
+            )
+            .formLogin(login ->
+                login
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .failureUrl("/faq")
+                .permitAll()
+            )
+            .logout(logout -> logout.logoutSuccessUrl("/").permitAll()
+        );
 
-public class WebSecurityConfig extends WebSecurityConfiguration {
-
+        return http.build();
+    }  
 }
