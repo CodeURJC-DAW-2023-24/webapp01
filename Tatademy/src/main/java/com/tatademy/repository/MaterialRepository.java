ce46508eaac3d6fa61bcaf01dc269d1c570c3dc3
jesussmariscal <mariscalalonso16@icloud.com>
2024-03-02 22:07:41 +0100

    Algorithm implementation separated from everything.

--- Tatademy/src/main/java/com/tatademy/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tatademy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tatademy.model.Material;
-
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-
-}
--- Tatademy/src/main/java/com/tatademy/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tatademy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tatademy.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
