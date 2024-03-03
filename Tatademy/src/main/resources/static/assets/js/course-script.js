dc5322b6fb9866f65a3b4134de78c7a3c121d4e4
jesussmariscal <mariscalalonso16@icloud.com>
2024-03-03 22:38:22 +0100

    Big fixed users

--- Tatademy/src/main/resources/static/assets/js/course-script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var form = document.getElementById('search-form');
-	var orderBy = document.getElementById('options');
-	orderBy.addEventListener('change', function () {
-		form.submit(); 
-	});
-});
