e360ee6b1e2ff9d8d98b4d68b145667ff3d27d73
p-torrecilla <p.torrecilla.2021@alumnos.urjc.es>
2024-03-03 07:20:00 +0100

    Showing profile information

--- Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
+++ Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
@@ -1,8 +1,10 @@
 package com.tatademy.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.Base64;
 
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -11,7 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.tatademy.model.User;
+import com.tatademy.repository.UserRepository;
 import com.tatademy.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +28,9 @@ public class UserLoggedController {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@ModelAttribute
 	public void addAttributes(Model model, HttpServletRequest request) {
@@ -43,8 +52,38 @@ public class UserLoggedController {
 	}
 
 	@GetMapping("/user/setting-edit-profile")
-	public String settingEditProfile() {
+	public String settingEditProfile(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		User user = userRepository.findByEmail(principal.getName());
+		model.addAttribute("surname", user.getSurname());
+		model.addAttribute("image", user.getImage());
+		model.addAttribute("image_file", user.getImageFile());
+		model.addAttribute("id", user.getId());
+		model.addAttribute("email", user.getEmail());
 		return "setting-edit-profile";
+	}
+
+	@PostMapping("/user/update-profile")
+	public String updateProfile(@RequestParam Long id, @RequestParam("fileImage") MultipartFile fileImage,
+			@RequestParam String name, @RequestParam String surname) throws IOException {
+		User user = userRepository.findById(id).orElseThrow();
+		user.setName(name);
+		user.setSurname(surname);
+		if (fileImage.getSize() != 0 ) {
+		user.setImage(null);
+		user.setImageFile(BlobProxy.generateProxy(fileImage.getInputStream(), fileImage.getSize()));
+			}
+		userRepository.save(user);
+		return "redirect:/user/setting-edit-profile";
+	}
+
+	@PostMapping("/user/delete-image")
+	public String deleteImage(@RequestParam Long id){
+		User user = userRepository.findById(id).orElseThrow();
+		user.setImageFile(null);
+		user.setImage(null);
+		userRepository.save(user);
+		return "redirect:/user/setting-edit-profile";
 	}
 
 	@GetMapping("/user/setting-student-security")
