162b25e82ef6a3bad4858c5396cc508b9ad60279
PaulaRuizRubio <paula.ruizrubio2003@gmail.com>
2024-03-03 19:17:07 +0100

    Statistics page (finished)

--- Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
+++ Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
@@ -3,6 +3,8 @@ package com.tatademy.controller;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Base64;
+import java.util.Calendar;
+import java.util.List;
 
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.tatademy.model.Course;
+import com.tatademy.model.Review;
 import com.tatademy.model.User;
+import com.tatademy.repository.CourseRepository;
+import com.tatademy.repository.ReviewRepository;
+import com.tatademy.repository.UserRepository;
 import com.tatademy.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -32,6 +40,19 @@ public class UserLoggedController {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private ReviewRepository reviewRepository;
+    @Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private CourseRepository courseRepository;
+
+
+
+
+
+
 
 	@ModelAttribute
 	public void addAttributes(Model model, HttpServletRequest request) {
@@ -129,5 +150,74 @@ public class UserLoggedController {
 		logoutHandler.logout(request, null, null);
 		return "redirect:/";
 	}
+
+
+
+
+
+
+	@GetMapping("/TatademyStadict") 
+	public String tatadyStadistic(Model model) {
+
+		//total reviews and stars average
+		model.addAttribute("usersEnroll", userRepository.findUsersEnrolledInAtLeastOneCourse().size());
+		List<Review> allReviews = reviewRepository.findAll();
+		Double starAverage = 0.0;
+		for (Review review : allReviews) {
+			starAverage += review.getStarsValue();
+		}
+		model.addAttribute("TotalReviews", allReviews.size());
+		model.addAttribute("starsAverage", starAverage/(allReviews.size()));
+		
+		return "instructor-dashboard";
+    }
+
+
+	@GetMapping("/cursesMonth")
+    @ResponseBody
+    public Integer[] CursesMonthUntilOct() {
+        List<Course> allCourses = courseRepository.findAll();
+		Integer[] coursesByMonth = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+		for (Course course : allCourses) {
+			Calendar creationDate = course.getCreationDate();
+			int month = creationDate.get(Calendar.MONTH);
+			
+			if (month >= 0 && month <= 9){
+				coursesByMonth[month]++;
+			}			
+		}
+	
+		for (int i = 0; i < coursesByMonth.length; i++) {
+			int cantidadCursos = coursesByMonth[i];
+			System.out.println("/cursesMonth  -> Month " + (i + 1) + ": " + cantidadCursos + " courses created");
+		}
+		
+		return coursesByMonth;
+    }
+
+	@GetMapping("/reviewsMonth")
+    @ResponseBody
+    public Integer[] ReviewsMonthUntilSept() {
+        List<Review> allreviews = reviewRepository.findAll();
+		Integer[] reviewsByMonth = {0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+
+		for (Review review : allreviews) {
+			Calendar creationDate = review.getCreationDate();
+			int month = creationDate.get(Calendar.MONTH);
+			if (month >= 0 && month <= 8){
+				reviewsByMonth[month]++;
+			}
+		}
+		for (int i = 0; i < reviewsByMonth.length; i++) {
+			int cantidadReview = reviewsByMonth[i];
+			System.out.println("/reviewsMonth  -> Month " + (i + 1) + ": " + cantidadReview + " reviews created");
+		}
+		return reviewsByMonth;
+    }	
+
+
+
+
 
 }
