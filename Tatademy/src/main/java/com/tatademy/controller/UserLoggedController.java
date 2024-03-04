14f712d8d481dc1ca15cb8c9976c21351c257e7b
jesussmariscal <mariscalalonso16@icloud.com>
2024-03-04 08:29:22 +0100

    delete unused imports

--- Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
+++ Tatademy/src/main/java/com/tatademy/controller/UserLoggedController.java
@@ -6,8 +6,6 @@ import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
